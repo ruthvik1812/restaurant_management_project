dd6a8dd86b67b45132cec3a20f3a91f5bcad7229
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-08-08 20:54:57 +0530

    27139

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Product ,Category ,Order,OrderItem,Table
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(Order Item)
+admin.site.register(Table)
