1669a286222266d5333414d9ace2d1917083ce42
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-08-10 19:10:55 +0530

    27142

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -128,3 +128,4 @@ STATICFILES_DIRS =[ BASE_DIR
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+RESTAURANT_NAME ="Campus Cafe"
