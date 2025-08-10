94ced8e792a628afa870fe4400ecc7158b63ec94
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-08-10 15:25:28 +0530

    27139

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'restaurant_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,8 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS =[ BASE_DIR
+    / "static", ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
