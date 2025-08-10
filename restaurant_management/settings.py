5691b2b1d41627d36006c1342691c8b332855e07
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-08-10 16:24:31 +0530

    27140

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -61,7 +60,7 @@ ROOT_URLCONF = 'restaurant_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,10 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[ os.path.join(BASE_DIR
-    products / 'static'), ]
+STATICFILES_DIRS =[ BASE_DIR
+    / "static", ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
