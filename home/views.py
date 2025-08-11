6f14d2926a5deaed3eb6e7394d3593911a7607fe
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-08-11 17:47:32 +0530

    29060

--- home/views.py
+++ home/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 from djang.shortcuts import render
 
-def homepage(request):
+def home(request):
     return HttpResponse("home.html")
    
