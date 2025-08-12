d07e261f2d7b8dbd6d840e24b02fb022d5847d52
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-08-12 08:03:02 +0530

    29062

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class RestaurantInfo(models.model):
+    name = models.charfield(max_length=255)
+    phone = models.chharfield(max_length=15)
