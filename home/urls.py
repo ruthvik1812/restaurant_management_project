dbbea17a67f9dacdbc5ce616554ab3fdddb55b61
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-08-15 21:46:12 +0530

    33713

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,7 @@ from . import views.
 
 urlpatterns = [
     path('',views.home,name ='home'),
-    path('contact/',views.contact,name='contact'),
+    path('contact/',views.contact,name='con(tact'),
+    path('staff/login/',staff_login, name='staff-login'),
     
 ]
