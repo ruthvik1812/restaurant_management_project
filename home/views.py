dea5e32fc630cf6b9656e67c81fe0df9e7c30aee
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-08-15 21:44:42 +0530

    33713

--- home/views.py
+++ home/views.py
@@ -8,3 +8,22 @@ def home(request):
     'restaurant_name':settings.RESTAURANT_NAME,
     'phone_number':settings.ReSTAURANT_PHONE
     })
+@api_view(['post'])
+def staff_login(request):
+    try:
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+
+        if not email or not password:
+            return Response(
+                {'error':'Email and password are required'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            staff = Staff.objects.filter(email=email).first()
+            if staff and check_password(password,staff.password):
+                return Response({'message':'Login successful'}, status=status.HTTP_200_OK)
+            else:
+                return Responce({'error':'Invalid credintials'}, status=status.HTTP_401_UNAUTHORIZED)
+        except Exeception as e:
+            return Response({'error':str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
