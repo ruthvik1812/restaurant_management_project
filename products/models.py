18956217a5f43927edabaa496fdd90ee3d161899
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-08-18 18:11:27 +0530

    34671

--- products/models.py
+++ products/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    item_name = models.CharField(max_length=150)
-    item_price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-
+    name = models.CharField(max_length=150)
+    description = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=8,decimal_places=2)
+    created_at =
     def __str__(self):
         return str(self.item_name)
+
+class Order(models..Model):
+    customer_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    items = models.ManyTOManyField(Menu)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer_name}"
