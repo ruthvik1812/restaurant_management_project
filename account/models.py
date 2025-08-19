fdba10623ca18333f9b47a4ca2e89ad84bf927fd
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-08-19 20:39:23 +0530

    34668

--- account/models.py
+++ account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+# Create your models here
+class UserProfile(models.model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
 
-# Create your models here.
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+
+    def __str__(self):
+        return self.user.get_full_name() 
