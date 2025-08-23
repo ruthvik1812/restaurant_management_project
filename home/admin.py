6348d1e96483812ad934b6f9421fee78e253ad37
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-08-23 14:58:59 +0530

    37946

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import MenuItem
 # Register your models here.
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name','description', 'price')
