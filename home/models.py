8988b341d25e8b0bf8dc763514bd3668edc64486
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-08-23 15:40:32 +0530

    37944

--- home/models.py
+++ home/models.py
@@ -68,3 +68,10 @@ class Menu(models.Model):
 
     def __str__(self):
         return f"{self.name} - ₹{self.price}"
+    
+    class Contact(models.Model):
+        name = models.CharField(max_length=100)
+        email = models.EmailField()
+        created_at = models.DataTimeField(auto_now_add=True)
+        def __str__(self):
+            return self.name
