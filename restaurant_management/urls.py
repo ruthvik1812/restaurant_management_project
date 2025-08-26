a3d7265557bdc538d77202e74747ba3358e8917b
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-08-26 20:53:19 +0530

    39043

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -18,11 +18,17 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/',include('home.urls')),
-    path('api/accounts/',include('account.urls')),
+    path('api/','accounts/',include('account.urls')),
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
-    path('',include('home.urls')),
-    path('appi/menu/', get_menu, name='menu'),
+    
+    # Menu API Endpoint
+    path('api/menu/', get_menu, name='menu'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings..MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])  
+     
