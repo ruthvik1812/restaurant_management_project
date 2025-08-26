b3db02d32a64e99fee708f527bfd145f3fcdf0e3
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-08-26 20:15:43 +0530

    39043

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -106,6 +106,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#=================Email Settings================
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ruthvikraj.chintala1812@gmail.com'
+EMAIL_HOST_PASSWORD = 'ruthvik@18'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
