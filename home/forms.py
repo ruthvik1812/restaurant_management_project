cea15766d7b7fd10e7f3849e3289c6d67705e25f
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-08-27 13:06:34 +0530

    39702

--- home/forms.py
+++ home/forms.py
@@ -4,4 +4,25 @@ from .models import Contact
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ['name', 'email']
+        fields = ['name', 'email', 'message']
+        widgets = {
+            'name':forms.TextInput(attrs={'form-control', 'placeholder': 'Enter your name'})
+            'email': forms.EmailIntput(attr={'class': 'form-control', 'placeholder': 'Enter your email'}),
+            'message': forms.Textarea(attrs={'class': 'forms-control', 'placeholder': 'Type your mesaage  here', 'rows': 4}),
+
+        }
+    # Validate Email Field
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not email:
+            raise forms.ValidationError("Email is required!")
+        if "@" not in email or "." not in email:
+            raise forms.ValidationError("Enter a valid email address!")
+            return email
+    # Validate Message Field
+    def clean_message(self):
+        message = self.cleaned_data.get('message')
+        if not message or len(message.strip()) < 10:
+            raise forms.ValidationError("Message must be atleast 10 characters long.")
+        return message3
+        
