f97862a1d16605ac0aeef7eb6de718ee0d098611
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-09-05 12:04:10 +0530

    42549

--- orders/views.py
+++ orders/views.py
@@ -2,4 +2,46 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import MenuItem
 
 # Create your views here.
-# Home V
+# Home view
+def home(request):
+    menu_items = MenuItem.objects.all()
+    
+    # Get cart from session
+
+    cart = request.session.get('cart',{})
+    cart_items = []
+    total = 0
+    for item_id, quantity in cart.items():
+        item = get_object_or_404(MenuItem, id=item_id)
+        cart_items.append({
+            'id': item.id,
+            'price': item.price,
+            'quantity': quantity,
+            'total_price': item.price * quantity
+        })
+        total += item..price * quantity
+    
+    context = {
+        'menu_items': menu_items,
+        'cart_items': cart_items,
+        'total': total
+    }
+    return render(request, 'home.html', context)
+
+    # Add item to cart
+def add_to_cart(request, item_id):
+    cart = request.session.get('cart',{})
+    cart[str(item_id)] = cart.get(str(item_id), 0) + 1
+    request.session['cart'] = cart
+return redirect('home')
+
+# Update item quantity in cart
+def update_cart(request, item_id):
+    quantity = int(request.POST.get('quantity', 1))
+    cart = request.session.get('cart',{})
+    if quantity > 0:
+        cart[str(item_id)] = quantity
+    else: 
+        cart.pop(str(item_id), None)
+    request.session['cart'] = cart
+return redirect('home')
