30155269ac29f348b7affd4fb0140adc52778aae
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-09-05 12:12:38 +0530

    42549

--- home/urls.py
+++ home/urls.py
@@ -13,7 +13,9 @@ urlpatterns = [
     path('order/', views.order_page,name='home'),
     path('login/', views.logout_view, name='login'),
     path('logout/'),views.logout_view, name='logout'),
-    
+    path('add-to-cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('update-cart/<int:item_id>/', views.update_cart, name='update_cart'),
 ]
 
 if settings.DEBUG:
