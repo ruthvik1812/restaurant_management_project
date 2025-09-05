ab5029dc8e1948df0cd68b92b8e2a48d277da85d
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-09-05 14:40:39 +0530

    42547

--- products/models.py
+++ products/models.py
@@ -16,3 +16,17 @@ class Order(models..Model):
 
     def __str__(self):
         return f"Order #{self.id} by {self.customer_name}"
+
+class TodaysSpecial(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Special Item Name")
+    description = models.TextField(verbose_name="Special Item Description", blank=True, null=True) 
+    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name="price")
+    image =  models.ImageField(upload_to="specials/", blank=True, verbose_name="Special Dish Image")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Meta:
+    verbose_name = "Today's special"
+    verbose_name_pural ="Today's Specials"
+    ordering = [-'created_at']
+def __str__(self):
+    return self.name
