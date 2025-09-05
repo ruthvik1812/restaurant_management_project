deb1d412abb3e759d7a19e03dd59ac534dba4860
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-09-05 14:46:13 +0530

    42547

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('menu/', views.menu_list, name='menu-list'),
+    path('menu/', views.menu_list, name='home'),
+    path(''),views.home, name='home'),
 ]
