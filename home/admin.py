3872bf5c9acaab6b17f7bc29a6f4cc047ba5a95b
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-09-06 11:19:46 +0530

    43019

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,36 @@
 from django.contrib import admin
-from .models import MenuItem
+from .models import Restaurant, MenuItem, Contact, RestaurantLocation, Feedback
 # Register your models here.
 
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name','owner_name', 'email', 'phone_number', 'city')
+    search_fields = ('name', 'owner_name', 'city')
+    list_filter =('city',)
+    ordering = ('-created_at')
+
+#---------MENU ITEM ADMIN ----------#
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ('name','description', 'price')
+    list_display = ('name', 'description', 'price','restaurant')
+    search_fields = ('name','descrption')
+    list_filter = ('restaurant',)
+    ordering =('name',)
+# ------ Contact Admin ------------ #
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name','email','created_at')
+    search_fields = ('name', 'email')
+    ordering = ('-created_at',)
+# ------------- RESTAURANT LOCATION ADMIN --------------#
+@admin.register(RestaurantLocation)
+class RestaurantLocation(admin.ModelAdmin):
+    list_display = ('name', 'comment', 'submitted_at')
+    search_fields = ('name', 'comment')
+    list_filter =('city', 'state')
+#--------------------- FEEDBACK ADMIN ----------------#
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('name','comment', 'submitted_at')
+    search_fields = ('name', 'comment')
+    ordering = ('-submitted_at',)
