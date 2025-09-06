5cbb4fd362aa8daca804524ccc3c03e98a66ad02
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-09-06 17:35:23 +0530

    43016

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,8 @@ from . import views.
 
 urlpatterns = [
     path('',views.home,name ="home"),
-    path("contact/",views.contact,name="home"),
+    path("contact/",views.contact,name="contact"),
+    path("thank-you/", views.thank_you, name="thank_you"),
     path("feedback/",views.submit_feedback, name="home"),
     path("staff/login/",staff_login, name="home"),
     path("reservation/",view.reservations ,name="home"),
@@ -15,8 +16,9 @@ urlpatterns = [
     path('logout/'),views.logout_view, name='logout'),
     path('add-to-cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove-from-cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
-    path('update-cart/<int:item_id>/', views.update_cart, name='update_cart'),
-]
+    path('update-cart/<int:item_id>/', views.update_cart, name='update_cart')
+    
+    ]
 
 if settings.DEBUG:
     urlpatterns += Static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
