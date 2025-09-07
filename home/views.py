6cf55741c8b3a36e74b73a49b4b245cd07215c69
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-09-07 19:03:47 +0530

    43158

--- home/views.py
+++ home/views.py
@@ -10,6 +10,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .forms import ContactForm, FeedbackForm
+from .models import RestaurantInfo
 # Create your views here.
 
 #==========Home page View==========#
