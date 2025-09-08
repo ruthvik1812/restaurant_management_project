42f5050aa0c32dbdf0db8bf1df10af544462f563
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-09-08 19:04:09 +0530

    43325

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'yourappname.context_processors.current_year',
+                'your_app.context_processors.current_year',
             ],
         },
     },
