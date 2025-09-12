62bf0fcd294d47babc79d209e970a48831e7b723
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-09-12 21:37:15 +0530

    44179

--- home/urls.py
+++ home/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     path('update-cart/<int:item_id>/', views.update_cart, name='update_cart'),
     path("about/", views.about, name="about"),
     path('chefs/', views.chefs, name='chefs'),
+    path('reservations/', views.reservations, name='reservation'),
     ]
 
 if settings.DEBUG:
