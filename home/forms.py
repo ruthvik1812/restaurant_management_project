eb73e697e16e881f31102dc788e5778dc5de301e
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-09-16 18:07:06 +0530

    44175

--- home/forms.py
+++ home/forms.py
@@ -6,11 +6,12 @@ class ContactForm(forms.ModelForm):
         model = Contact
         fields = ['name', 'email', 'message']
         widgets = {
-            'name':forms.TextInput(attrs={'form-control', 'placeholder': 'Enter your name'})
-            'email': forms.EmailIntput(attr={'class': 'form-control', 'placeholder': 'Enter your email'}),
-            'message': forms.Textarea(attrs={'class': 'forms-control', 'placeholder': 'Type your mesaage  here', 'rows': 4}),
+            'name':forms.TextInput(attrs={'form-control', 'placeholder': 'Enter your name'}),
+            'email': forms.EmailIntput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'}),
+            'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Type your mesaage  here', 'rows': 4}),
 
         }
+    
     # Validate Email Field
     def clean_email(self):
         email = self.cleaned_data.get('email')
@@ -18,19 +19,21 @@ class ContactForm(forms.ModelForm):
             raise forms.ValidationError("Email is required!")
         if "@" not in email or "." not in email:
             raise forms.ValidationError("Enter a valid email address!")
-            return email
+        return email
     # Validate Message Field
     def clean_message(self):
         message = self.cleaned_data.get('message')
         if not message or len(message.strip()) < 10:
             raise forms.ValidationError("Message must be atleast 10 characters long.")
         return message
-class ReservationForm(forms.ModelForm):
-    class Meta:
-        model = Reservation
-        fields = ['name','email', 'phone','date','time','guests', 'notes']
-        widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'}),
-            'time':forms.TimeInput(attrs={'type':'time'}),
-            
-        }
+    # Newsletter subscription Form
+    class NewsletterForm(forms.ModelForm):
+        class Meta:
+            model = NewsletterSubscriber
+            fields = ['email']
+            widgets = {
+                'email': forms.EmailIntput(attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Enter your email to subscribe'
+                }),
+            }
