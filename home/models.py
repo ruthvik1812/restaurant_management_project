9e808dad8c0be5145643767e9d9b3f924c01f2f7
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-09-16 18:09:47 +0530

    44175

--- home/models.py
+++ home/models.py
@@ -155,3 +155,9 @@ class chef(models.Model):
 
     def __str__(self):
         return self.name
+class NewsletterSubscriber(models.Model):
+    email = models.EmailField(unique=True)
+    subscribed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
