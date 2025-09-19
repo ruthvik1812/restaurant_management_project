4f355fac3ba2516a08ba3b9f54c73584cdac3876
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-09-19 20:38:07 +0530

    48618

--- orders/models.py
+++ orders/models.py
@@ -56,3 +56,12 @@ class Order(models.Model):
 
         def __str__(self):
             return f"{self.quantity)} * {self.product.name} (Order {self.order.id})"
+        
+        class Coupon(models.Model):
+            code = models.CharField(max_length=20, unique=True)
+            discount = models.DecimalField(max_digits=5, decimal_places=2)
+            active = models.BooleanField(default=True)
+            created_at = models.DateTimeField(auto_now_add=True)
+
+            def __str__(self):
+                return self.code
