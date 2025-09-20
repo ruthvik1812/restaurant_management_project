905124e8d8efb3dac5c1b1ce728a822baf478fda
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-09-20 10:03:30 +0530

    49721

--- home/serializers.py
+++ home/serializers.py
@@ -5,3 +5,12 @@ class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuCategory
         fields = ['id', 'name']
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ["id", "name", "description", "price", "available"]
+
+    def validate_price(self, value):
+        if value <=0:
+            raise serializers.validationError("Price must be a positive number.")
+        return value
