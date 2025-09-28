8928988d1e7f21ebc6182a8f95a4e0fdb5e53b5f
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-09-28 16:03:01 +0530

    51653

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
 from .models import Order, OrderItem
+from home.models import Product
+from account.models import User
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'price']
 
 class OrderItemSerializer(serializers.ModelSerializer):
     product_name = serializers.CharField(source="product.name", read_only=True)
@@ -10,8 +17,9 @@ class OrderItemSerializer(serializers.ModelSerializer):
         fields = ["id", "product_name", "product_price", "quantity"]
 
 class OrderSerializer(serializers.ModelSerializer):
+    customer = serializers.StringRelatedField()
     items = OrderItemSerializer(many=True, read_only=True)
 
     class Meta:
         model = Order
-        fields = ["id", "created_at", "total_amount", "items"]
+        fields = ["order_id", "customer", "items", "total_price", "created_at"]
