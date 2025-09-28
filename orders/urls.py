1a8e85af898c711ab3e2e2bd8daeb3516333273f
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-09-28 16:11:17 +0530

    51653

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('menu/',,views.menu_list,name='menu_list'),
     path("history/", OrderHistoryView.as_view(), name="order-history"),
+    path('orders/<str:order_id/', OrderDetailView.as_view(), name='order-detail'),
 ]
