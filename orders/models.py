1381cb2d5b618e76a10130a172ce8108f6867c61
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-10-06 19:24:10 +0530

    52152

--- orders/models.py
+++ orders/models.py
@@ -24,6 +24,7 @@ class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
     order_items = models.ManyToManyField(product, through='OrderItem')
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
     
     objects = models.manager()
