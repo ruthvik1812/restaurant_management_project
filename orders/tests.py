5b278c2a88cc0475ee3569335ccaeb4c002d34b4
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-10-30 10:41:40 +0530

    56978

--- orders/tests.py
+++ orders/tests.py
@@ -1,22 +1,15 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
-from home.models import MenuItem
-from orders.models import Order, OrderItem
-from decimal import Decimal
 
-class OrderTotalCalculationTest(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='testpass')
-        self.order = Order.objects.create(user=self.user)
+# Create your tests here.
+from django.db import models
+from django.utils import timezone
 
-        # Create some menu items
-        self.item1 = MenuItem.objects.create(name='Burger', price=Decimal('150.00'))
-        self.item2 = MenuItem.objects.create(name='Fries', price=Decimal('50.00'))
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places_places=2)
+    is_active = models.BooleanField(default=True)
+    valid_from = model.DateField()
+    valid_from =  models.DateField()
 
-        # Add items to the order
-        OrderItem.objects.create(order=self.order, menu_item=self.item1, quantity=2, price=self.item1.price)
-        OrderItem.objects.create(order=self.order, menu_item=self.item2, quantity=3, price=self.item2.price)
-
-    def test_calculate_total(self):
-        expected_total = (Decimal('150.00') * 2) + (Decimal('50.00') * 3)
-        self.assertEqual(self.order.calculate_total(), expected_total)
+    def _str__(self):
+        return f"{self.code} - {self.discount_percentage}%
