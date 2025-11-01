df1950d96079796cfeb6fe06cdf3e77883e766a3
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-11-01 19:39:06 +0530

    57557

--- home/models.py
+++ home/models.py
@@ -209,4 +209,4 @@ class NutritionalInformation(models.Model):
     kjjjkkjgjhhggggghgh
     kjjjjjgkjghasdfghjk
     wsdfghjklfhsfgjsghkmghdfcghbjnkmjliohjgv bmnmbnmbghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhv
-    cfbvvvvvvvvvvvvvvvcgggggggghjvbvvvvvvvvvjnjjbbbbbbbbbbbbjvvbj
+    cfbvvvvvvvvvvvvvvvcgggggggghjvbvvvvvvvvvjnjjbbbbbbbbbbbbjvvbjhukkkkkkkkkkkkkkkkkkkkkj
