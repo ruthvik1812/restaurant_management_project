f4a569c44f09402b91ba3520591fc76688968bda
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-11-01 19:42:32 +0530

    57608

--- home/models.py
+++ home/models.py
@@ -205,8 +205,4 @@ class NutritionalInformation(models.Model):
     def __str__(self):
         return f"{self.menu_item.name} - {self.calories} kcal"
 
-    kjbmmmmmmmmmmkk
-    kjjjkkjgjhhggggghgh
-    kjjjjjgkjghasdfghjk
-    wsdfghjklfhsfgjsghkmghdfcghbjnkmjliohjgv bmnmbnmbghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhv
-    cfbvvvvvvvvvvvvvvvcgggggggghjvbvvvvvvvvvjnjjbbbbbbbbbbbbjvvbjhukkkkkkkkkkkkkkkkkkkkkj
+    
