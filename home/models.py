152eaff7770c0576cb320cfb2494dac34de3fafd
ruthvik1812 <ruthvikraj.chintala1812@gmail.com>
2025-11-01 18:39:17 +0530

    56977

--- home/models.py
+++ home/models.py
@@ -191,3 +191,16 @@ class NewsletterSubscriber(models.Model):
 
     def __str__(self):
         return self.email
+class NutritionalInformation(models.Model):
+    menu_item  = models.ForeignKey(
+        'MenuItem',
+        on_delete=models.CASCADE,
+        related_name='nutritional_info'
+    )
+    calories = models.IntegerField()
+    protein_grams = models.DecimalField(max_digits=5, decimal_places=2)
+    fat_grams = models.DecimalField(max_digits=5, decimal_places=2)
+    carbohydrate_grams = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.menu_item.name} - {self.calories} kcal"
